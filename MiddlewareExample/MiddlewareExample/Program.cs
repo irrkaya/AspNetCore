d7522e32ccc8b5d7bdd74b7b7f10be87292fde9e
Irem (Irene) Soyaker <irem_95@windowslive.com>
2023-03-07 19:47:53 +1100

    Middleware class added.

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -1,12 +1,36 @@
+using MiddlewareExample.CustomMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
-app.Run(async (HttpContext context ) => {
-    await context.Response.WriteAsync("Hello");
+//middleware 1
+app.Use(async (HttpContext context, RequestDelegate next ) => {
+    await context.Response.WriteAsync("From middleware 1");
+    await next(context);
 });
 
+
+//middleware 2
+//app.Use(async (HttpContext context, RequestDelegate next) => {
+//    await context.Response.WriteAsync("Hello again");
+//    //await next(context);
+//});
+
+//middleware 2
+//    app.Use(async (context, next) => {
+//    await context.Response.WriteAsync("Hello again");
+//    await next(context);
+//});
+
+
+//middleware 2
+app.UseMiddleware<MyCustomMiddleware>();
+
+
+//middleware 3
 app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello again");
+    await context.Response.WriteAsync("From middleware 3");
 });
 
 app.Run();
