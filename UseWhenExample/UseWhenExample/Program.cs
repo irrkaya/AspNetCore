75cc1d127bae3b6237abd8b65a4641c072544ed7
Irem (Irene) Soyaker <irem_95@windowslive.com>
2023-03-13 18:50:39 +1100

    UseWhen Example

---
+++ UseWhenExample/UseWhenExample/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("username"),
+    app =>
+    {
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello from " +
+                "middleware branch");
+            await next();
+        });
+    }
+    );
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello from middleware at main chain");
+
+});
+
+app.Run();
